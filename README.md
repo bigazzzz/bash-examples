0089456ef044449aa05998f59f7d381189623325
bigazzzz <bigazzzz@gmail.com>
2023-04-10 20:37:34 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# bash script examples
+
+## links
+
+https://levelup.gitconnected.com/5-modern-bash-scripting-techniques-that-only-a-few-programmers-know-4abb58ddadad
+
+
---
+++ frame_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while true;
+do
+    # Frame #1
+    printf "\r< Loading..." 
+    sleep 0.5
+    # Frame #2 
+    printf "\r> Loading..." 
+    sleep 0.5 
+done
